8ebe041d9244cfd3e105f8a3c6c7c41048e00110
lukashenkkko <76600990+lukashenkkko@users.noreply.github.com>
2023-03-29 13:49:39 +0300

    Add files via upload

---
+++ codewars/codewars.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+
+std::string likes(const std::vector<std::string>& names)
+{
+	switch (names.size())
+	{
+	case 0:return "no one likes this"; break;
+	case 1:return names[0] + " likes this"; break;
+	case 2:return names[0] + " and " + names[1] + " like this"; break;
+	case 3:return names[0] + ", " + names[1] + " and " + names[2] + " like this"; break;
+	default: return names[0] + ", " + names[1] + " and " + std::to_string(names.size() - 2) +" others like this"; break;
+	}
+}
+
+int main()
+{
+	std::vector<std::string> names = { "Snegana" ,"Kambucha","Igor","Karina","Yulia","Semen"};
+
+	std::cout<<likes(names);
+}
